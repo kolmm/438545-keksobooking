c71392ab46e3094fd71f604ee7761b71d0261d3c
Kolmogorov Maxim <kolmm3@gmail.com>
2018-02-14 19:22:35 +0500

    add pin module

--- js/map.js
+++ js/map.js
@@ -1,31 +1,5 @@
 'use strict';
 
-var AVATAR_WIDTH = 40;
-var AVATAR_HEIGHT = 40;
-
-function renderPin(pin) {
-  var pinTemplate = document.querySelector('template').content.querySelector('.map__pin');
-  var mapPin = pinTemplate.cloneNode(true);
-  var fragment = document.createDocumentFragment();
-  var imgPin = mapPin.querySelector('img');
-
-  mapPin.className = 'map__pin';
-  mapPin.style.left = pin.location.x - 50 / 2 + 'px';
-  mapPin.style.top = pin.location.y - 70 + 'px';
-  imgPin.width = AVATAR_WIDTH;
-  imgPin.height = AVATAR_HEIGHT;
-  imgPin.src = pin.author.avatar;
-
-  mapPin.appendChild(imgPin);
-  fragment.appendChild(mapPin);
-
-  mapPin.addEventListener('click', function (evt) {
-    onPinClick(evt, pin);
-  });
-
-  return fragment;
-}
-
 var mainObject = window.getMainObject(8);
 var map = document.querySelector('.map');
 var noticeForm = document.querySelector('.notice__form');
@@ -35,12 +9,6 @@ var mapPinMain = document.querySelector('.map__pin--main');
 fieldSet.forEach(function (field) {
   field.disabled = true;
 });
-
-function onPinClick(evt, pin) {
-  var mapFilters = document.querySelector('.map__filters-container');
-
-  map.insertBefore(window.renderCard(pin), mapFilters);
-}
 
 window.setAddress(true);
 
@@ -53,7 +21,7 @@ function makePageActive() {
     field.disabled = false;
   });
   mainObject.forEach(function (object) {
-    mapPins.appendChild(renderPin(object));
+    mapPins.appendChild(window.renderPin(object));
   });
 }
 
---
+++ js/pin.js
@@ -0,0 +1,36 @@
+'use strict';
+
+(function () {
+  var AVATAR_WIDTH = 40;
+  var AVATAR_HEIGHT = 40;
+  var map = document.querySelector('.map');
+
+  function onPinClick(evt, pin) {
+    var mapFilters = document.querySelector('.map__filters-container');
+
+    map.insertBefore(window.renderCard(pin), mapFilters);
+  }
+
+  window.renderPin = function (pin) {
+    var pinTemplate = document.querySelector('template').content.querySelector('.map__pin');
+    var mapPin = pinTemplate.cloneNode(true);
+    var fragment = document.createDocumentFragment();
+    var imgPin = mapPin.querySelector('img');
+
+    mapPin.className = 'map__pin';
+    mapPin.style.left = pin.location.x - 50 / 2 + 'px';
+    mapPin.style.top = pin.location.y - 70 + 'px';
+    imgPin.width = AVATAR_WIDTH;
+    imgPin.height = AVATAR_HEIGHT;
+    imgPin.src = pin.author.avatar;
+
+    mapPin.appendChild(imgPin);
+    fragment.appendChild(mapPin);
+
+    mapPin.addEventListener('click', function (evt) {
+      onPinClick(evt, pin);
+    });
+
+    return fragment;
+  };
+})();
