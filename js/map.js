15de8060767b6cf6a765afd72b4d272e579af4b5
Kolmogorov Maxim <kolmm3@gmail.com>
2018-02-16 11:23:32 +0500

    map.js in ,odule

--- js/map.js
+++ js/map.js
@@ -1,39 +1,41 @@
 'use strict';
 
-var mainObject = window.getMainObject(8);
-var map = document.querySelector('.map');
-var noticeForm = document.querySelector('.notice__form');
-var fieldSet = noticeForm.querySelectorAll('fieldset');
-var mapPinMain = document.querySelector('.map__pin--main');
+(function () {
+  var mainObject = window.getMainObject(8);
+  var map = document.querySelector('.map');
+  var noticeForm = document.querySelector('.notice__form');
+  var fieldSet = noticeForm.querySelectorAll('fieldset');
+  var mapPinMain = document.querySelector('.map__pin--main');
 
-fieldSet.forEach(function (field) {
-  field.disabled = true;
-});
-
-window.util.setAddress(true);
-
-function makePageActive() {
-  var mapPins = document.querySelector('.map__pins');
-
-  map.classList.remove('map--faded');
-  noticeForm.classList.remove('notice__form--disabled');
   fieldSet.forEach(function (field) {
-    field.disabled = false;
+    field.disabled = true;
   });
-  mainObject.forEach(function (object) {
-    mapPins.appendChild(window.renderPin(object));
-  });
-}
 
-function onMapClick() {
-  if (map.classList.contains('map--faded')) {
-    makePageActive();
-    window.util.disableCapacity();
-    window.util.makeMinPrice();
-    window.util.capacity.options[2].selected = true;
+  window.util.setAddress(true);
+
+  function makePageActive() {
+    var mapPins = document.querySelector('.map__pins');
+
+    map.classList.remove('map--faded');
+    noticeForm.classList.remove('notice__form--disabled');
+    fieldSet.forEach(function (field) {
+      field.disabled = false;
+    });
+    mainObject.forEach(function (object) {
+      mapPins.appendChild(window.renderPin(object));
+    });
   }
 
-  window.util.setAddress(false);
-}
+  function onMapClick() {
+    if (map.classList.contains('map--faded')) {
+      makePageActive();
+      window.util.disableCapacity();
+      window.util.makeMinPrice();
+      window.util.capacity.options[2].selected = true;
+    }
 
-mapPinMain.addEventListener('mouseup', onMapClick);
+    window.util.setAddress(false);
+  }
+
+  mapPinMain.addEventListener('mouseup', onMapClick);
+})();
